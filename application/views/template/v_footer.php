f51351b940b72423de58eff0e26000796e413846
nocturnalcoders <nocturnalcoders@github.com>
2021-11-10 13:44:07 +0700

    recode table

--- application/views/template/v_footer.php
+++ application/views/template/v_footer.php
@@ -77,12 +77,59 @@
                 }
             });
 
+            var table = $('#example1').DataTable({
+                "responsive": true,
+                "order": [
+                    [1, "desc"]
+                ],
+                "oLanguage": {
+                    "sInfo": "Showing _START_ to _END_ of _TOTAL_ items."
+                }
+            });
 
+            $("#example1 tfoot th").each(function(i) {
 
+                if ($(this).text() !== '') {
+                    var isStatusColumn = (($(this).text() == 'Status') ? true : false);
+                    var select = $('<select><option value=""></option></select>')
+                        .appendTo($(this).empty())
+                        .on('change', function() {
+                            var val = $(this).val();
 
+                            table.column(i)
+                                .search(val ? '^' + $(this).val() + '$' : val, true, false)
+                                .draw();
+                        });
 
+                    // Get the Status values a specific way since the status is a anchor/image
+                    if (isStatusColumn) {
+                        var statusItems = [];
+
+                        /* ### IS THERE A BETTER/SIMPLER WAY TO GET A UNIQUE ARRAY OF <TD> data-filter ATTRIBUTES? ### */
+                        table.column(i).nodes().to$().each(function(d, j) {
+                            var thisStatus = $(j).attr("data-filter");
+                            if ($.inArray(thisStatus, statusItems) === -1) statusItems.push(thisStatus);
+                        });
+
+                        statusItems.sort();
+
+                        $.each(statusItems, function(i, item) {
+                            select.append('<option value="' + item + '">' + item + '</option>');
+                        });
+
+                    }
+                    // All other non-Status columns (like the example)
+                    else {
+                        table.column(i).data().unique().sort().each(function(d, j) {
+                            select.append('<option value="' + d + '">' + d + '</option>');
+                        });
+                    }
+
+                }
+            });
         });
     </script>
+
     </body>
 
     </html>
--- application/views/v_perubahan.php
+++ application/views/v_perubahan.php
@@ -4,7 +4,7 @@
         <div class="panel panel-default">
             <div class="panel-body">
                 <div class="table-responsive">
-                    <table class="table table-striped table-bordered table-hover" id="example">
+                    <table class="table table-striped table-bordered table-hover" id="example1">
                         <thead>
                             <tr>
                                 <th>Nama Kecamatan</th>
